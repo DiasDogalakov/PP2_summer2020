09324f8690d49f02b1d355a11ca81cee5ecdacf3
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-06-18 00:45:41 +0600

    Update C.py

--- Contest(16.06.20)/C.py
+++ Contest(16.06.20)/C.py
@@ -1,6 +1,7 @@
-a = int(input())
-b = int(input())
+a, b = input().split()
+a = int(a)
+b = int(b)
 
 for i in range(a, b):
     if(i % 7 == 1 or i % 7 == 2 or i % 7 == 5):
-        print(i)
+        print(i, end = " ")
