535a72e3635a0ac83729eb6133c135a5ba536de2
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-06-19 22:13:57 +0600

    Create 2.py

---
+++ week 2 day 2/JSON/2.py
@@ -0,0 +1,13 @@
+#Parse JSON - Convert from JSON to Python
+If you have a JSON string, you can parse it by using the json.loads() method.
+
+import json
+
+# some JSON:
+x =  '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
