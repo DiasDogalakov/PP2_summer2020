f66a1a76087ad08b634a21c330f1e4a2b60141c6
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-06-25 23:38:34 +0600

    Create 7.py

---
+++ week 2 day 1/7.py
@@ -0,0 +1,9 @@
+#Accessing Items
+
+#You can access the items of a dictionary by referring to its key name, inside square brackets:
+
+x = thisdict["model"]
+
+#There is also a method called get() that will give you the same result:
+
+x = thisdict.get("model")
