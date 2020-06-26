a285555a516de538123242bce18d48a0b47920c9
DiasDogalakov <60038731+DiasDogalakov@users.noreply.github.com>
2020-06-26 17:13:20 +0600

    Create 2.py

---
+++ TSIS 5/2.py
@@ -0,0 +1,11 @@
+import re
+s = input()
+
+def function(s):
+        patterns = 'ab*'
+        if re.search(patterns, s):
+                return True
+        else:
+                return False
+
+print(function(s))
